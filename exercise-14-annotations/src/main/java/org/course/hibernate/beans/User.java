a65fbdafea6a1d0d64372090b0d2c7c34671c4e9
Javier Izquierdo Gomez <javier.izquierdo.gomez@gmail.com>
2020-01-01 23:56:08 +0100

    more exercises

---
+++ exercise-14-annotations/src/main/java/org/course/hibernate/beans/User.java
@@ -0,0 +1,58 @@
+package org.course.hibernate.beans;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class User implements Serializable {
+
+	private static final long serialVersionUID = -5192213898097992882L;
+
+	@Id
+    @GeneratedValue
+	private Long id;
+
+	private String name;
+
+	private String password;
+
+	/**
+	 * new instance
+	 */
+	public User() {
+
+	}
+
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		return EqualsBuilder.reflectionEquals(this, obj, false);
+	}
+
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this, false);
+	}
+}
